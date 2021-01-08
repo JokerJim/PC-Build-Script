17ab9d46afb3d2639588c181c21c418b87397db0
JokerJim <JokerJim@users.noreply.github.com>
2021-01-08 17:11:24 -0500

    Update PCSetup.ps1

--- PCSetup.ps1
+++ PCSetup.ps1
@@ -737,11 +737,11 @@ function LayoutDesign {
         Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
         Exit
     }
-    Import-StartLayout -LayoutPath "c:\build\PC-Build-Script-master\LayoutModification.xml" -MountPath $env:SystemDrive\
+    Import-StartLayout -LayoutPath "c:\Pirum\PC-Build-Script-master\LayoutModification.xml" -MountPath $env:SystemDrive\
     }
     
 function ApplyDefaultApps {
-    dism /online /Import-DefaultAppAssociations:c:\build\PC-Build-Script-master\AppAssociations.xml
+    dism /online /Import-DefaultAppAssociations:c:\Pirum\PC-Build-Script-master\AppAssociations.xml
 }
 
 # Custom power profile used for our customers. Ensures systems do not go to sleep.
