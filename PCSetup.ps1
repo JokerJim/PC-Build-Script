ece573d9708e70153db0e95d09e66d86afdf01fc
JokerJim <JokerJim@users.noreply.github.com>
2022-07-24 22:08:10 -0400

    Update PCSetup.ps1
    Fixed personalization. Added checks for existing files, reorganized so that the commands are together for the same part of the process (so profile pics commands are all together and background commands are all together instead of copying commands all together then all registry commands)

--- PCSetup.ps1
+++ PCSetup.ps1
@@ -775,78 +775,84 @@ function personalize{
 	$OSDISK = $boot.driveletter + ":"
 
 	$SID=[System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
-	$Wallpaper = "backgroundDefault.jpg"
+
+	# OEM bitmap
 	$OEMLogo = "OEMLogo.bmp"
-	$UserBMP32 = "user32.bmp"
-	$UserBMP40 = "user40.bmp"
-	$UserBMP48 = "user48.bmp"
-	$UserBMP96 = "user96.bmp"
-	$UserBMP192 = "user192.bmp"
-	$UserBMP240 = "user240.bmp"
-	$LockScreen = "lockscreen.jpg"
-
-	# copy the OEM bitmap
-	If (-not(Test-Path "c:\windows\system32\oobe\info\backgrounds")){New-item "c:\windows\system32\oobe\info\backgrounds" -type directory}
-
-	copy-item "$OSDISK\Pirum\media\$OEMLogo" "$OSDISK\windows\system32"
-	copy-item "$OSDISK\Pirum\media\$UserBMP32" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
-	copy-item "$OSDISK\Pirum\media\$UserBMP40" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
-	copy-item "$OSDISK\Pirum\media\$UserBMP48" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
-	copy-item "$OSDISK\Pirum\media\$UserBMP96" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
-	copy-item "$OSDISK\Pirum\media\$UserBMP192" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
-	copy-item "$OSDISK\Pirum\media\$UserBMP240" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
-	copy-item "$OSDISK\Pirum\media\$OEMLogo" "$OSDISK\windows\system32\oobe\info"
-	copy-item "$OSDISK\Pirum\media\$Wallpaper" "$OSDISK\windows\system32\oobe\info\backgrounds"
-	copy-item "$OSDISK\Pirum\media\$Wallpaper" "$OSDISK\Windows\Web\Screen"
-	copy-item "$OSDISK\Pirum\media\$Wallpaper" "$OSDISK\Windows\Web\Wallpaper\Windows"
-	copy-item "$OSDISK\Pirum\media\$LockScreen" "$OSDISK\Windows\System32"
-
-	# make required registry changes
 	$strPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\OEMInformation"
-	$strPath2 = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background"
-	$subPath3 = "HKLM:\Software\Policies\Microsoft\Windows"
-	$strPath3 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"
-	$subPath4 = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies"
-	$strPath4 = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
-	$subPath5 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users"
-	$strPath5 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users\$SID"
-	$subPath6 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+	if(Test-Path -Path "$OSDISK\Pirum\defmedia\$OEMLogo") {
+		copy-item "$OSDISK\Pirum\defmedia\$OEMLogo" "$OSDISK\windows\system32"
+		copy-item "$OSDISK\Pirum\defmedia\$OEMLogo" "$OSDISK\windows\system32\oobe\info"
+		Set-ItemProperty -Path $strPath -Name Logo -Value "$OSDISK\Windows\System32\OEMlogo.bmp"
+	}
 
 	#set OEM information
-	Set-ItemProperty -Path $strPath -Name Logo -Value "$OSDISK\Windows\System32\OEMlogo.bmp"
 	Set-ItemProperty -Path $strPath -Name Manufacturer -Value "Pirum Consulting LLC"
 	Set-ItemProperty -Path $strPath -Name SupportPhone -Value "330-597-0450"
 	Set-ItemProperty -Path $strPath -Name SupportHours -Value "Mon-Fri 9am-5pm"
 	Set-ItemProperty -Path $strPath -Name SupportURL -Value "http://pirumllc.itclientportal.com"
-	Set-ItemProperty -Path $strPath2 -Name OEMBackground -value 1
 	
-	#set lockscreen
-    if(!(Test-Path $subPath3)) {
-		New-Item -Path $subPath3 -Name "Personalization" -Force
+	
+	# background
+	$Wallpaper = "backgroundDefault.jpg"
+	$strPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+	if(Test-Path -Path "$OSDISK\Pirum\media\$Wallpaper") {
+		If (-not(Test-Path "c:\windows\system32\oobe\info\backgrounds")){New-item "c:\windows\system32\oobe\info\backgrounds" -type directory}
+		copy-item "$OSDISK\Pirum\media\$Wallpaper" "$OSDISK\windows\system32\oobe\info\backgrounds"
+		copy-item "$OSDISK\Pirum\media\$Wallpaper" "$OSDISK\Windows\Web\Screen"
+		copy-item "$OSDISK\Pirum\media\$Wallpaper" "$OSDISK\Windows\Web\Wallpaper\Windows"
+
+		New-Item -Path $subPath -Name "System" -Force
+		Set-ItemProperty -Path $strPath -Name Wallpaper -value "$OSDISK\Windows\Web\Wallpaper\Windows\$Wallpaper"
+		Set-ItemProperty -Path $strPath -Name WallpaperStyle -value "2"
+	
 	}
-	Set-ItemProperty -Path $strPath3 -Name LockScreenImage -value "$OSDISK\Windows\Web\Screen\$Wallpaper"
-
-    if(!(Test-Path $subPath6)) {
-        New-Item -Path $subPath6 -Force | Out-Null
-    }
-	New-ItemProperty -Path $subPath6 -Name LockScreenImageStatus -Value 1 -PropertyType DWORD -Force | Out-Null
-	New-ItemProperty -Path $subPath6 -Name LockScreenImagePath -Value "$OSDISK\Windows\System32\$LockScreen" -PropertyType STRING -Force | Out-Null
-	New-ItemProperty -Path $subPath6 -Name LockScreenImageUrl -Value "$OSDISK\Windows\System32\$LockScreen" -PropertyType STRING -Force | Out-Null
 	
+	#lockscreen
+	$LockScreen = "lockscreen.jpg"
+	if(Test-Path -Path "$OSDISK\Pirum\media\$LockScreen") {
+		copy-item "$OSDISK\Pirum\media\$LockScreen" "$OSDISK\Windows\System32"
 
-	#set wallpaper
-	New-Item -Path $subPath4 -Name "System" -Force
-	Set-ItemProperty -Path $strPath4 -Name Wallpaper -value "$OSDISK\Windows\Web\Wallpaper\Windows\$Wallpaper"
-	Set-ItemProperty -Path $strPath4 -Name WallpaperStyle -value "2"
-	
-	#set user icon for lockscreen
-	New-Item -Path $subPath5 -Name "$SID" -Force
-	Set-ItemProperty -Path $strPath5 -Name Image32 -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\$UserBMP32"
-	Set-ItemProperty -Path $strPath5 -Name Image40 -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\$UserBMP40"
-	Set-ItemProperty -Path $strPath5 -Name Image48 -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\$UserBMP48"
-	Set-ItemProperty -Path $strPath5 -Name Image96 -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\$UserBMP96"
-	Set-ItemProperty -Path $strPath5 -Name Image192 -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\$UserBMP192"
-	Set-ItemProperty -Path $strPath5 -Name Image240 -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\$UserBMP240"
+		$strPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System"
+		Set-ItemProperty -Path $strPath -Name UseOEMBackground -value 1
+
+		$strPath = "HKLM:\Software\Policies\Microsoft\Windows"
+		if(!(Test-Path "$strPath\Personalization")) {
+			New-Item -Path $strPath -Name "Personalization" -Force
+		}
+		Set-ItemProperty -Path "$strPath\Personalization" -Name LockScreenImage -value "$OSDISK\Windows\System32\$LockScreen"
+
+		$strPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion"
+		if(!(Test-Path "$strPath\PersonalizationCSP")) {
+			New-Item -Path $strPath -Name "PersonalizationCSP" -Force
+		}
+		New-ItemProperty -Path $subPath6 -Name LockScreenImageStatus -Value 1 -PropertyType DWORD -Force
+		New-ItemProperty -Path $subPath6 -Name LockScreenImagePath -Value "$OSDISK\Windows\System32\$LockScreen" -PropertyType STRING -Force
+		New-ItemProperty -Path $subPath6 -Name LockScreenImageUrl -Value "$OSDISK\Windows\System32\$LockScreen" -PropertyType STRING -Force
+	}
+
+
+	# user account pictures
+	$sizelist = "32","40","48","96","192","240"
+	if(Test-Path -Path "$OSDISK\Pirum\media\user.png"){
+		copy-item "$OSDISK\Pirum\media\user.png" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
+	}
+	if(Test-Path -Path "$OSDISK\Pirum\media\user.bmp"){
+		copy-item "$OSDISK\Pirum\media\user.bmp" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
+	}
+
+	$strPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users"
+	New-Item -Path $strPath -Name "$SID" -Force
+	$strPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users\$SID"
+	foreach($size in $sizelist){
+		if(Test-Path -Path "$OSDISK\Pirum\media\user-$size.bmp"){
+			copy-item "$OSDISK\Pirum\media\user-$size.bmp" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
+			Set-ItemProperty -Path $strPath -Name "Image$size" -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\user-$size.bmp"
+		}
+		if(Test-Path -Path "$OSDISK\Pirum\media\user-$size.png"){
+			copy-item "$OSDISK\Pirum\media\user-$size.png" "$OSDISK\ProgramData\Microsoft\User Account Pictures"
+			Set-ItemProperty -Path $strPath -Name "Image$size" -value "$OSDISK\ProgramData\Microsoft\User Account Pictures\user-$size.png"
+		}
+	}
 }
 
 function RestartPC{
